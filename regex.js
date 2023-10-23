6c9e04100e99a39829a1ef15a3cd225c6743dc6b
Emily Slade <60137462+emilyslade123@users.noreply.github.com>
2023-10-23 04:19:26 +0100

    Create overview page

--- regex.js
+++
@@ -1,9 +0,0 @@
-export const validId = (str) => {
-    const newStr = str.replaceAll(/([\s.\+\-\/0-9])/g, '');
-    return newStr;
-}
-
-export const capitaliseEachWord = (str) => {
-    const newStr = str.replaceAll(/(?<!\b)(\w)/g, d => d.toLowerCase());
-    return newStr;
-}
